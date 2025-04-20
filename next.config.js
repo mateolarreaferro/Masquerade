4a474e05a8d37e5a313602a186a47578aedb11f7
gabe magaña <gdmagana@stanford.edu>
2025-04-19 17:28:58 -0700

    symlink frontend dir

---
+++ next.config.js
@@ -0,0 +1,3 @@
+// This file is needed to run Next.js from the root directory
+// It simply re-exports the configuration from the frontend directory
+module.exports = require('./frontend/next.config.ts');
